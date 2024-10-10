a90b570fad9a366e0a8b2982ce2daab335ca0a7b
Dominik Konaszynski <dkonaszynski5@gmail.com>
2024-10-10 14:51:33 +0200

    Sekcja 5

---
+++ 02-TypesAndVariables/5-5.py
@@ -0,0 +1,13 @@
+price_string = input('Enter the price : ')
+price = float(price_string)
+price = round(price,2)
+discount_string = input('Enter the discount % : ')
+discount = int(discount_string)
+price_after_discount = price * (1 - discount/100)
+price_after_discount = round(price_after_discount,2)
+reduction = price - price_after_discount
+reduction = round(reduction,2)
+print(f'Enter price : {price}')
+print(f'Enter discount % : {discount}')
+print(f'Price with discount : {price_after_discount} ')
+print(f'Reduction : {reduction}')
