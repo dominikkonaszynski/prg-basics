576b3d9ec2eeffa2ac8b13bc8716d54e1d4fbf3b
Dominik Konaszynski <dkonaszynski5@gmail.com>
2024-10-13 10:51:51 +0200

    5

--- 02-TypesAndVariables/5-4.py
+++ 02-TypesAndVariables/5-4.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
+
 amount_string = input('Enter the amount : ')
 amount = float(amount_string)
 vat = amount * 0.23
 vat = round(vat,2)
 print(f'Amount : {amount}')
 print(f'VAT 23% : {vat} ')
-=======
-# A program to calculate VAT from amount
-amount_string = input('Enter the amount : ')
-amount = float(amount_string)
-vat = amount * 0.23
-vat = round(vat,2)
-print(f'Amount : {amount}')
-print(f'VAT 23% : {vat} ')
->>>>>>> 16db95deb71fcc5869014aa704a5472427c20d8c
+
