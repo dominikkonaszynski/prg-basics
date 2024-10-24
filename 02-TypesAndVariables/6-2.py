e03dc74e603e9937b6a1c433171154d7e3ca3180
Dominik Konaszynski <dkonaszynski5@gmail.com>
2024-10-24 23:37:37 +0200

    6

---
+++ 02-TypesAndVariables/6-2.py
@@ -0,0 +1,6 @@
+###
+# A program that prints your initials
+#
+name = 'Dominik'
+surname = 'Konaszyński'
+print(f"Your initials are {name[0]}{surname[0]}")
---
+++ 02-TypesAndVariables/6-6.py
@@ -0,0 +1,7 @@
+###
+# A program to print numerical representations of characters.
+#
+print(f'a {ord('a')}')
+print(f'b {ord('b')}')
+...
+...
