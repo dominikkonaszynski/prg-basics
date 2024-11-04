17def88884fa3ac97ad77c2f4fb5fe98cefbe697
Dominik Konaszynski <dkonaszynski5@gmail.com>
2024-11-04 23:21:52 +0100

    7

--- 04-Functions/7-23.py
+++ 04-Functions/7-23.py
@@ -1,7 +1,7 @@
 def f(expression):
     total = 0
     current_number = ''
-    operation = '+'  # Start with addition
+    operation = '+'
 
     for char in expression:
         if char.isdigit():
@@ -11,12 +11,11 @@ def f(expression):
             current_number = ''
             operation = char
 
-    # Handle the last number
     total += int(current_number) if operation == '+' else -int(current_number)
 
     return total
 
 if __name__ == "__main__":
-    print(f("2+3"))         # Should return 5
-    print(f("3+8+1"))       # Should return 12
-    print(f("2+3-4+5-0"))   # Should return 6
+    print(f("2+3"))         
+    print(f("3+8+1"))       
+    print(f("2+3-4+5-0"))  
