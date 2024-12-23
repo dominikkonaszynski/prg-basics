704e5d86b43b7e279ddf2a2e4554e1f2930e08ea
Dominik Konaszynski <dkonaszynski5@gmail.com>
2024-12-23 17:20:58 +0100

    4

--- 11-ObjectOrientedProgramming/tv.py
+++ 11-ObjectOrientedProgramming/tv.py
@@ -52,3 +52,19 @@ class TV:
         else:
             self.volume -= amount
             print(f"Volume decreased to {self.volume}.")
+
+def main():
+    tv1 = TV()
+    tv1.show_status()
+    tv1.turn_on()
+    tv1.show_status()
+    tv1.show_channels()
+    tv1.set_channels(["TVP1", "TVP2", "Polsat", "TVN", "Filmbox", "Discovery"])
+    tv1.show_channels()
+    tv1.set_channel(1)
+    tv1.increase_volume(3)
+    tv1.show_status()
+    tv1.turn_off()
+    
+if __name__ == "__main__":
+    main()
